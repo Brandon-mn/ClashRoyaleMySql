91f89beda2ce8886e41cac0f82413b2b4604c910
DELL LATITUDE <DELL LATITUDE@DESKTOP-EJQ756D>
2022-12-09 19:47:15 -0600

    Agregar archivos de proyecto.

---
+++ ClashRoyaleSqlServer/conexinsqlserver.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ClashRoyaleSqlServer
+{
+    class conexinsqlserver
+    {
+        public static MySqlConnection conexion = new MySqlConnection();
+        static string servidor = "127.0.0.1";
+        static string bd = "clashroyale";
+        static string ususario = "root";
+        static string password = "5770210";
+        static string puerto = "3375";
+
+
+        static String cadenaConexion = "server=" + servidor + ";" + "user id =" + ususario + ";" + "password=" + password + ";" + "port= " + puerto + ";" + "database=" + bd + ";";
+
+        private static void conectar()
+        {
+            try
+            {
+                conexion.ConnectionString = cadenaConexion;
+                conexion.Open();
+                //MessageBox.Show("Se conecto correctamente la base de datos");
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("No se puede conectar a la base de datos de PostgreSQL, error:" + e.ToString());
+            }
+
+        }
+        public static DataTable ejecutaConsultaSelect(string consulta)
+        {
+            conectar();
+            MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, conexion);
+            DataSet ds = new DataSet();
+            adaptador.Fill(ds, "tabla");
+            conexion.Close();
+            return ds.Tables["tabla"];
+        }
+        public static void ejecutaConsulta(string consulta)
+        {
+            conectar();
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+}
